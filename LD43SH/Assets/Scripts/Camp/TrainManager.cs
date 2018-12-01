ec44e4eec35ac1dfece660ad0a451f83be470cb6
Kathlar <thomass1@wp.pl>
2018-12-01 13:14:39 +0100

    Train Manager + Spline Editor

---
+++ LD43SH/Assets/Scripts/Camp/TrainManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TrainManager : MonoBehaviour
+{
+    protected CampResources resources;
+    public List<TrainScenario> TrainScenarios;
+
+    void Awake()
+    {
+        resources = GetComponent<CampResources>();
+    }
+
+    public void TrainSpawn()
+    {
+        //create train object
+    }
+
+    public void TrainArrive()
+    {
+        //train arrives on station
+        TrainScenario scenario = TrainScenarios[0];
+        resources.food.value += scenario.food;
+        //spawn people
+        if (TrainScenarios.Count > 1) TrainScenarios.RemoveAt(0);
+    }
+}
+
+
+[CreateAssetMenu(menuName = "Train Scenario")]
+public class TrainScenario : ScriptableObject
+{
+    public float food;
+    public int numberOfPeople;
+}
