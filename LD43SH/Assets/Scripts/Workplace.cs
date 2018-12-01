f9648e5c4ca002a25c2b52f18b348bb1471d18bb
Jakub Rogalski <ringlan@gmail.com>
2018-12-01 05:04:10 -0800

    worker logic update

---
+++ LD43SH/Assets/Scripts/Workplace.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//placeholder
+public class Workplace : MonoBehaviour
+{
+    public ActionData[] waypoints;
+    public List<Worker> workers;
+
+    private void Start()
+    {
+        foreach (Worker worker in WorkerManager.workers)
+            worker.StartWorking(this);
+    }
+
+    public ActionData GetAction()
+    {
+        return waypoints[Random.Range(0, waypoints.Length)];
+    }
+
+    //periodically update production, handle null, dead workers
+}
+
+[System.Serializable]
+public class ActionData
+{
+    public string animName;
+    public float time;
+    public Transform place;
+}
