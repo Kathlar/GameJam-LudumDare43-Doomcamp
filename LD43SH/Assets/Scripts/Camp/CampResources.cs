f1663b56d449aa44b4828fb35fb1c9e6824ca757
PilotPix <piotrkulla@gmail.com>
2018-12-01 15:29:20 +0100

    Workplace resources gaining

--- LD43SH/Assets/Scripts/Camp/CampResources.cs
+++ LD43SH/Assets/Scripts/Camp/CampResources.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class CampResources : MonoBehaviour
 {
@@ -43,6 +44,11 @@ public class CampResources : MonoBehaviour
 
         numberOfPeopleText.text = "Workers\n" + numberOfPeople.ToString();
         numberOfGuardsText.text = "Guards\n" + numberOfGuards.ToString();
+    }
+
+    public Resource GetResource(ResourceType resourceType)
+    {
+        return Resources.Single(resource => resource.resourceType == resourceType);
     }
 
     public void DailyUseOfResources()
