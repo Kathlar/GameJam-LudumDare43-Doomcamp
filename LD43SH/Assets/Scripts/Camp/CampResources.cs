83550c7a1273a947a268fbbab75dc03d3ac74126
Kathlar <thomass1@wp.pl>
2018-12-01 12:48:03 +0100

    Basic Resources

---
+++ LD43SH/Assets/Scripts/Camp/CampResources.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampResources : MonoBehaviour
+{
+    public Resource food = new Resource(ResourceType.Food, 100, 10);
+    public Resource morale = new Resource(ResourceType.Morale, 50, 0);
+    public Resource metal = new Resource(ResourceType.Metal, 50, 0);
+    public Resource stone = new Resource(ResourceType.Stone, 50, 0);
+    public Resource wood = new Resource(ResourceType.Wood, 50, 0);
+
+    public int numberOfPeople;
+    public int numberOfGuards;
+
+    void Start()
+    {
+        CountPeople();
+    }
+
+    public void DailyUseOfResources()
+    {
+        food.DailyUsage();
+        morale.DailyUsage();
+        metal.DailyUsage();
+        stone.DailyUsage();
+        wood.DailyUsage();
+    }
+
+    void CountPeople()
+    {
+        //numberOfGuards = ;
+        numberOfPeople = WorkerManager.workers.Count;
+    }
+}
+
+[Serializable]
+public class Resource
+{
+    public ResourceType resourceType;
+    public float value;
+    public float dailyUsage = 0;
+
+    public Resource(ResourceType rType, float startValue, float dailyUsage = 0)
+    {
+        resourceType = rType;
+        value = startValue;
+        this.dailyUsage = dailyUsage;
+    }
+
+    public void DailyUsage()
+    {
+        value = Mathf.Clamp(value - dailyUsage, 0, Mathf.Infinity);
+    }
+}
+
+public enum ResourceType
+{
+    Food, Morale, Metal, Wood, Stone
+}
---
+++ LD43SH/Assets/Scripts/Camp/DayTimeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTimeManager : MonoBehaviour
+{
+    protected CampResources campResources;
+
+    public int dayNumber = 1;
+    public float dayLength = 120;
+
+    void Awake()
+    {
+        campResources = GetComponent<CampResources>();
+    }
+
+    void Start()
+    {
+        InvokeRepeating("ChangeDay", dayLength, dayLength);
+    }
+
+    public void ChangeDay()
+    {
+        dayNumber++;
+        campResources.DailyUseOfResources();
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 100), dayNumber.ToString());
+    }
+}
