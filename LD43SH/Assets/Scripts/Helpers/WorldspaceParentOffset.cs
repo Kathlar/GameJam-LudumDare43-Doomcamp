310404efc5230c0905b7544b80ee45f377544fad
Jakub Rogalski <ringlan@gmail.com>
2018-12-02 11:52:40 +0100

    added ui prompt above slacking and running workers

---
+++ LD43SH/Assets/Scripts/Helpers/WorldspaceParentOffset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldspaceParentOffset : MonoBehaviour
+{
+    public Vector3 offset = Vector3.up;
+
+    private void Update()
+    {
+        transform.position = transform.parent.position + offset;   
+    }
+}
---
+++ LD43SH/Assets/Scripts/UI/WorkerMoodUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerMoodUI : MonoBehaviour
+{
+    Worker worker;
+
+    public GameObject slackPrompt;
+    public GameObject runPrompt;
+
+    private void Awake()
+    {
+        worker = GetComponent<Worker>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateState", Random.Range(0.0f, 1.0f), 1.0f);
+    }
+
+    private void OnDestroy()
+    {
+        Cleanup();
+    }
+
+    void UpdateState()
+    {
+        if (worker.curState == WorkerState.slacking)
+        {
+            slackPrompt.SetActive(true);
+        }
+        else if (worker.curState == WorkerState.running)
+        {
+            runPrompt.SetActive(true);
+        }
+        else
+        {
+            slackPrompt.SetActive(false);
+            runPrompt.SetActive(false);
+        }
+    }
+
+    void Cleanup()
+    {
+        if (slackPrompt) Destroy(slackPrompt);
+        if (runPrompt) Destroy(runPrompt);
+    }
+}
